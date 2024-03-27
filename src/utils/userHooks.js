0db1c295704c7d8ce5e0a97a13b12f67c6ec0a27
momilette <charlotte@anros.fr>
2024-03-27 14:09:26 +0100

    mise en place du useContext pour garder l'ID

---
+++ src/utils/userHooks.js
@@ -0,0 +1,11 @@
+import { useContext } from "react"
+import { UserContext } from "./contexts"
+
+/**
+ * 
+ * @returns {number} User id
+ */
+export function useUserId() {
+    const user = useContext(UserContext);
+    return user.userId;
+}
