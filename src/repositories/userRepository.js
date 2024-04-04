cc58195059781325cf521bc6bd0c9bd1e4db3c26
momilette <charlotte@anros.fr>
2024-04-04 10:27:17 +0200

    mise en place repositories

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -16,7 +16,7 @@ function getUserData() {
  * @returns {number[]} user ids
  */
 export function getAllUserIds() {
-    const usersId = USER_MAIN_DATA.map(userId => userId.id);
+    const usersId = USER_MAIN_DATA.map((userId) => userId.id);
 
     return usersId;
 }
@@ -24,12 +24,31 @@ export function getAllUserIds() {
 /**
  * Get user infos from it's id
  * @param {number} id user id to fetch
- * @returns {UserData | undefined} data for the specified user or undefined
+ * @returns {UserInfos | undefined} data for the specified user or undefined
  */
 export function getUserInfos(id) {
     const usersData = getUserData();
-    const userData = usersData.find(userData => userData.id === id);
+    const userData = usersData.find((userData) => userData.id === id);
     const userInfos = userData?.userInfos;
-
+    console.log(userInfos);
     return userInfos;
 }
+
+// export class UsersData {
+//     // #route;
+//     // constructor(route) {
+//     //     this.route = route;
+//     // }
+
+//     async getUsersData() {
+//         const response = await Promise.resolve(USER_MAIN_DATA);
+
+//         return response;
+//     }
+
+//     async getUserData(id) {
+//         const usersData = await this.getUsersData();
+//         const userData = usersData.find((userData) => userData.id === id);
+//         return userData;
+//     }
+// }
